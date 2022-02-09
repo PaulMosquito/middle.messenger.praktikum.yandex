883ad992314333ecd0b22201586b5c803dd12b01
Komarov Pavel <komarov.p.se@gmail.com>
2022-02-10 00:27:59 +0300

    Added server, template & some components

---
+++ scripts/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = 3000;
+
+app.set('view engine', 'pug')
+app.set('views', path.resolve("src/pages"));
+
+const ROUTES = [
+	'auth',
+	'chat',
+	'login',
+	'profile',
+]
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+ROUTES.forEach(route => {
+	app.get(`/${route}`, (req, res) => {
+		res.render(route);
+	})
+});
+
+
+app.use((req, res) => {
+	res.status(400).render('404');
+	res.status(500).render('500');
+});
+
+
+
+app.listen(PORT);

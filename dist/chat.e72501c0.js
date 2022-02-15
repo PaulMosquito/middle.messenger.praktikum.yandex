8e3119651821cff9764e905dd0e2539303de095f
Komarov Pavel <komarov.p.se@gmail.com>
2022-02-15 17:56:24 +0300

    Updated gitignore & added netlify.toml

---
+++ dist/chat.e72501c0.js
@@ -0,0 +1,2 @@
+window.addEventListener("DOMContentLoaded",(function(){const n=document.getElementById("lenta-conversation");n.scrollTo(0,n.scrollHeight)}));
+//# sourceMappingURL=chat.e72501c0.js.map
---
+++ dist/profile.8232619b.js
@@ -0,0 +1,2 @@
+window.addEventListener("DOMContentLoaded",(function(){[...document.getElementsByClassName("modal-button")].forEach((t=>{t.addEventListener("click",function(t){const e=document.getElementById(t).lastChild;return t=>{e.style.display="block"===e.style.display?"none":"block",t.stopImmediatePropagation()}}(t.id))}))}));
+//# sourceMappingURL=profile.8232619b.js.map

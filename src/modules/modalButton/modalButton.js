af3956772341ed2c2e86f1632d156424d374e59b
Komarov Pavel <komarov.p.se@gmail.com>
2022-02-18 12:28:38 +0300

    Fixed some nitpicks

--- src/modules/modalButton/modalButton.js
+++ src/modules/modalButton/modalButton.js
@@ -1,7 +1,7 @@
 (function() {
     window.addEventListener("DOMContentLoaded", function() {
         function handleOpenModal(buttonId) {
-            const modal = document.getElementById(buttonId).lastChild;
+            const modal = document.getElementById(buttonId)?.lastChild;
         
             return (event) => {
                 modal.style.display = modal.style.display === "block" ? "none" : "block";   
